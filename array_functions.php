d35004d4fbae52a7c2d84510691fbbd53bcd0779
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2024-12-30 03:16:55 +0100

    Create array_functions.php

---
+++ array_functions.php
@@ -0,0 +1,15 @@
+
+
+// find 1st element of an array by callback
+$numbers = [1, 2, 3, 4, 5, 6];
+$firstMatch = array_find(
+    array: $numbers, 
+    callback: fn (int $number) => $number % 2 === 0
+);
+
+// find all elements of an array by callback
+$numbers = [1, 2, 3, 4, 5, 6];
+$allMatches = array_filter(
+    array: $numbers, 
+    callback: fn (int $number) => $number % 2 === 0
+);
